41f0329daae864de5cb77aad332b2d36412fc411
gbjnewman <gbjnewman@protonmail.com>
2022-06-06 17:48:24 +1000

    add completed challenge

---
+++ codeWars/js/countOfPositivesSumofNegatives.js
@@ -0,0 +1,33 @@
+// https://www.codewars.com/kata/576bb71bbbcf0951d5000044
+
+/*
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+function countPositivesSumNegatives(input) {
+  // your code here
+  if (input == null) {
+    return []
+  }else if (input.length == 0){
+    console.log(input.length)
+    return []
+  } else {
+  let pos = 0
+  let neg = 0
+  input.forEach(function(element){
+    if (element > 0){
+      pos = pos+1
+    } else if (element < 0){
+      neg = neg + element
+    }
+  })
+    return [pos, neg]
+  }
+}
