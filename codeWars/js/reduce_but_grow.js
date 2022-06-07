79a991a5a67a07c62a7901d48cd060966bbb503e
gbjnewman <gbjnewman@protonmail.com>
2022-06-07 17:57:24 +1000

    updated challenge

--- codeWars/js/reduce_but_grow.js
+++ codeWars/js/reduce_but_grow.js
@@ -1,6 +1,6 @@
 
 /* Description:
-
+https://www.codewars.com/kata/57f780909f7e8e3183000078
 Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 
 [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24 */
@@ -11,11 +11,15 @@ Given a non-empty array of integers, return the result of multiplying the values
 // My function
 
 function grow(x) {
-	var output = 1;
-	for (var i = 0; i < x.length; i++) {
-		output = output * x[i];
-	}
-	return output
+  var output = 1;
+  for (var i = 0; i < x.length; i++) {
+    output = output * x[i];
+  }
+  return output
+}
+//redux
+function grow(x){
+  return x.reduce((acc, cVal) => acc * cVal,1)
 }
 
 /* My node test case
